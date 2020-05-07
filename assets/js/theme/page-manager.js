b03966bc2e53e9f1b9e69f6fd6751115ec551376
Dan Leatherman <dleatherman@gmail.com>
2020-05-07 10:07:50 -0400

    mothers day campaign and product tracking for klaviyo

--- assets/js/theme/page-manager.js
+++ assets/js/theme/page-manager.js
@@ -22,5 +22,34 @@ export default class PageManager {
                 if (!pageYOffset) window.scrollTo(0, 1);
             }, 1000);
         });
+
+        if (context.template === 'pages/home') {
+            const cartUrl = 'https://peppertogether.com/cart.php';
+            const basket = $('.bundle-products').data('bundle-products');
+            const addMultipleItemsToCart = function () {
+                if (basket.length) {
+                    for (let i = 0; i < basket.length; i++) {
+                        $.ajax({
+                            type: 'GET',
+                            async: false,
+                            // eslint-disable-next-line prefer-template
+                            url: cartUrl + '?action=add&product_id=' + basket[i] + '&fastcart=1&ajaxsubmit=1',
+                            success: (data) => {
+                                const obj = JSON.parse($(data).html());
+                                if (!obj.success) {
+                                    // eslint-disable-next-line no-console
+                                    console.log('Error', obj);
+                                }
+                            },
+                        });
+                    }
+                }
+                window.location.replace(cartUrl);
+            };
+            $('.bundle-products').click(() => {
+                $(this).addClass('running');
+                addMultipleItemsToCart();
+            });
+        }
     }
 }
